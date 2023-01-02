021514dd23a615d0bb7dc1586bb208390ee00ecd
wujunhui <1223356407@qq,con>
2023-01-02 19:40:32 +0800

    【fix】优化header组件的展示

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -16,11 +16,9 @@ import RouterLink from "components/RouterLink";
 import MaxWidthWrapper from "components/maxWidthWrapper";
 import { ROUTES } from "src/constants";
 const Header = () => {
-  // 菜单是否打开
-  const [open, setOpen] = useState(false)
   return (
     <HeaderWapper>
-      <MaxWidthWrapper style={{display:"flex", alignItems:"center", justifyContent: "space-between"}} >
+      <MaxWidthWrapper style={{display:"flex", alignItems:"center", justifyContent: "space-between", paddingBottom: 0, paddingTop: 0}} >
         <HeaderLeft>
           <RouterLink href="/" >
             <LogoFirst>东方战虎</LogoFirst>.<LogoLast>辉</LogoLast>
